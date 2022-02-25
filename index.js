d4ae072c654336acb142dbb9ca10a6508ce578b1
Kadeem140 <Beckford.kadeem@yahoo.com>
2022-02-24 20:49:50 -0500

    MVP

--- index.js
+++ index.js
@@ -12,5 +12,15 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
+ return tutorials.map((sentence) =>{ //map over each element
+   const words = sentence.split(" ") //Split each thing in sentence at the spaces, words
+   const capitalWords = words.map((word) => { // map over each word
+     return word.charAt(0).toUpperCase() + word.slice(1) //uppercase the first letter of each word, then add the remainder of the word excluding the first letter
+   })
+   const repsonse = capitalWords.join(" ") // join the response at the spaces to make a whole sentence again.
+   return repsonse //return
+ }) 
+  
 }
+ 
+// 
